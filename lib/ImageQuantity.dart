a3c172ad2da8242f2180834956150391ec91c13d
jerardh <jerardhtomj@gmail.com>
2025-07-19 20:03:31 +0530

    Food Quantity slider event handling

--- lib/ImageQuantity.dart
+++ lib/ImageQuantity.dart
@@ -10,29 +10,41 @@ class ImageQunatity extends StatefulWidget {
 
 class ImageQuantityState extends State<ImageQunatity> {
   var _quantity = 0.3; //300 grams
+  TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return (Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-      SizedBox(
-        width: 250,
-        child: Slider(
-        value: _quantity,
-        activeColor: AppColors.primaryDarker,
-        inactiveColor: AppColors.divider,
-        onChanged: (double value) {
-          setState(() {
-            _quantity = value;
-          });
-        },
-      )
-      ),
-      SizedBox(
-        width: 40,
-        child: TextField(maxLength: 4,keyboardType: TextInputType.number,decoration: InputDecoration(counterText: "")),
-      )
-    ]));
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+              width: 230,
+              child: Slider(
+                value: _quantity,
+                activeColor: AppColors.primaryDarker,
+                inactiveColor: AppColors.divider,
+                onChanged: (double value) {
+                  setState(() {
+                    _quantity = value;
+                    _controller.text = (_quantity*1000).toStringAsFixed(2);
+                  });
+                },
+              )),
+          SizedBox(
+              width: 80,
+              child: TextField(
+                  controller: _controller,
+                  onChanged: (String value) {
+                    setState(() {
+                      _quantity = double.parse(value)/1000;
+                    });
+                  },
+                  maxLength: 4,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      counterText: "",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8)))))
+        ]));
   }
 }
